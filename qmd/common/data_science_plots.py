62dbd01137d4150ccda5895310d06ea4a58ddd89
Javier Pérez <surtich@gmail.com>
2026-05-15 20:09:37 +0200

    Creado tema de derivadas

---
+++ qmd/common/data_science_plots.py
@@ -0,0 +1,22 @@
+from plotnine import *
+
+def custom_points(color='#FF4136', fill='#FF851B'):
+     return geom_point(color=color, fill=fill, size=5, stroke=1.5)
+
+def clean_theme(xrange=(-3, 10), yrange=(-3, 10)):
+    return [
+    geom_hline(yintercept=0, color='#666666', size=1),
+    geom_vline(xintercept=0, color='#666666', size=1),
+    xlim(*xrange), 
+    ylim(*yrange),        
+    scale_x_continuous(expand=(0, 0), limits=xrange, breaks=None, labels=None),
+    scale_y_continuous(expand=(0, 0), limits=yrange, breaks=None, labels=None),
+    theme_minimal(),
+    theme(
+        panel_grid_major=element_blank(),
+        panel_grid_minor=element_blank(),
+        axis_text=element_blank(),
+        axis_ticks=element_blank()
+    ),
+    labs(x="", y="")
+]
