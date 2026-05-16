e1551d9b7e5e19cd054b6d4db4659018a015b36b
Javier Pérez <surtich@gmail.com>
2026-05-16 13:05:44 +0200

    Creado capítulo de regresión múltiple

--- qmd/common/data_science_df.py
+++ qmd/common/data_science_df.py
@@ -3,7 +3,8 @@ import pandas as pd
 people_df = pd.DataFrame({
     'nombre': ['Leo', 'Sara', 'Marta', 'Elena', 'Javier'],
     'horas_estudio': [0, 5, 4, 6, 7],
-    'notas': [2, 4, 6, 8, 7]
+    'notas': [2, 4, 6, 8, 7],
+    'sexo': ['M', 'F', 'F', 'F', 'M']
 })
 
 people_df.set_index('nombre', inplace=True)
