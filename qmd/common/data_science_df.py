e47d2aa5e2d2711fda060f819195d6c23645339e
Javier Pérez <surtich@gmail.com>
2026-05-16 18:56:01 +0200

    Creado tema Regresión logística

--- qmd/common/data_science_df.py
+++ qmd/common/data_science_df.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 people_df = pd.DataFrame({
-    'nombre': ['Leo', 'Sara', 'Marta', 'Elena', 'Javier'],
+    'nombre': ['Leo', 'Sara', 'Javier', 'Elena', 'Marta'],
     'horas_estudio': [0, 5, 4, 6, 7],
     'notas': [2, 4, 6, 8, 7],
-    'sexo': ['M', 'F', 'F', 'F', 'M']
+    'sexo': ['M', 'F', 'M', 'F', 'F']
 })
 
 people_df.set_index('nombre', inplace=True)
